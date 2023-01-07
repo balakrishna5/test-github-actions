19cac31f429f260eecd9e9a488bc19d9d53f63e1
balakrishna5 <krishuk1089@outlook.com>
2023-01-07 14:06:39 +0000

    Changes for publishing packages to GitHub

---
+++ index.test.js
@@ -0,0 +1,7 @@
+const calculator = require('./index');
+
+function testFunction(){
+    const result = calculator.add(1,2);
+    expect(result).toBe(3);
+}
+test('add two numbers and verify the result is correct',testFunction);
